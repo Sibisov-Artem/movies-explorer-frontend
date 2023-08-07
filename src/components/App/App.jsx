168e9077ecb5921a92485bf6680199b712b7163b
Artem Sibisov <artsib7777@yandex.ru>
2023-08-08 01:35:13 +0800

    Выполнить вертску Main - Promo для LANDING`а

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 
 import Header from "../Header/Header";
+import Main from "../Main/Main";
 
 function App() {
   return (
     <div className="root">
       <div className="page">
         <Header />
+        <Main />
       </div>
     </div>
   );
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import './Main.css';
+
+import Promo from '../Promo/Promo';
+
+function Main() {
+  return (
+    <main className='main'>
+      <Promo />
+    </main>
+  );
+}
+
+export default Main;
