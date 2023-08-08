b2bd56a3d9472da5e70abfbba16ac7f31ea7ecae
Artem Sibisov <artsib7777@yandex.ru>
2023-08-08 13:54:50 +0800

    fix: использовать тег списка ul в nav в Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,9 +5,11 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <nav className="navigation">
-      <Link to="/signup" className="navigation__link">Регистрация</Link>
-      <Link to="/signin" className="navigation__link navigation__link_signin">Войти</Link>
-    </nav>
+      <ul className='navigation__list'>
+        <li><Link to="/signup" className="navigation__link">Регистрация</Link></li>
+        <li><Link to="/signin" className="navigation__link navigation__link_signin">Войти</Link></li>
+      </ul>
+    </nav >
   );
 }
 
