eb4de84ecc74b568cd180fab555c186e0c50d3e3
Artem Sibisov <artsib7777@yandex.ru>
2023-08-08 11:33:58 +0800

    fix: изменить тег с ul на nav в Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <ul className="navigation">
-      <li><Link to="/signup" className="navigation__link">Регистрация</Link></li>
-      <li><Link to="/signin" className="navigation__link navigation__link_signin">Войти</Link></li>
-    </ul>
+    <nav className="navigation">
+      <Link to="/signup" className="navigation__link">Регистрация</Link>
+      <Link to="/signin" className="navigation__link navigation__link_signin">Войти</Link>
+    </nav>
   );
 }
 
