15051a117fc4e0311947527fa3ef706b34127cf4
Artem Sibisov <artsib7777@yandex.ru>
2023-08-09 00:37:19 +0800

    feat: выполнить верстку Main - AboutProject для LANDING`а

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import './Main.css';
 
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <main className="main">
       <Promo />
       <NavTab />
+      <AboutProject />
     </main>
   );
 }
