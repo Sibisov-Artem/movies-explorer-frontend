11ff898d05615c1c37e9764601295181e2dee557
Artem Sibisov <artsib7777@yandex.ru>
2023-08-09 23:01:32 +0800

    feat: выполнить верстку Main - AboutMe для LANDING`а

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,31 @@
+import './AboutMe.css';
+
+import aboutMeImage from '../../images/about-me.png';
+
+function AboutMe() {
+  return (
+    <section className="about-me section">
+
+      <h2 className="section__title">Студент</h2>
+
+      <div className="about-me__info">
+        <div className="about-me__description">
+          <h3 className="about-me__subtitle">Виталий</h3>
+          <p className="about-me__subline">Фронтенд-разработчик, 30 лет</p>
+          <p className="about-me__paragraph">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
+            и&nbsp;дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить.
+            С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке,
+            начал заниматься фриланс-заказами и ушёл с постоянной работы.
+          </p>
+          <a className="about-me__github" href="#">Github</a>
+        </div>
+
+        <img className="about-me__image" alt="Виталий" src={aboutMeImage}></img>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default AboutMe;
