b981f7c6d2ec552851182e6b8bfa1934b68d483a
Artem Sibisov <artsib7777@yandex.ru>
2023-08-12 23:30:56 +0800

    feat: создать логику для Header`a по установке цвета его бэкграунда в зависимости от маршрута по роутам

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,32 @@
 import './Header.css';
+import { useLocation } from 'react-router-dom';
 
 import Navigation from '../Navigation/Navigation';
 
+// Нужна логика, когда находмися в Main то бэкграунд темно-синий
+// В остальных случаях кроме 404, логин, регистрации (где хэдера нет вовсе) - хэдер
+// темно-серый. Нужно создать класс для хэдера для этого случая
+
 function Header() {
+
+  const location = useLocation();
+
   return (
-    <header className="header section">
-      <div className="header__logo"></div>
-      <Navigation />
-    </header>
+    <>
+      {location.pathname === "/" ||
+        location.pathname === "/movies" ||
+        location.pathname === "/saved-movies" ||
+        location.pathname === "/profile" ?
+        (
+          <header className={`header section header__background_theme_${location.pathname === "/" ? "main" : "other"}`}>
+            <div className="header__logo"></div>
+            <Navigation />
+          </header>
+        ) : (
+          null
+        )}
+    </>
+
   );
 }
 
