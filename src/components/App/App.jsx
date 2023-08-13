22555d28cf8a0a92dcaa9324bf59e2402630eba2
Artem Sibisov <artsib7777@yandex.ru>
2023-08-13 22:15:20 +0800

    feat: подключить в App.jsx Route маршрут по роуту "/movies" для компонента Movies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Main />} />
+          <Route path='/movies' element={<Movies />} />
         </Routes>
 
         <Footer />
---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+import './Movies.css'
+
+function Movies() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default Movies;
