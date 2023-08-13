a4a51cba4991670ada2670eeb40106f67d0cc65c
Artem Sibisov <artsib7777@yandex.ru>
2023-08-13 21:52:55 +0800

    feat: создать логику в Navigation для единого его отображния для роутов: /movies, /saved-movies, /profile и создать его отображение по макету

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,44 @@
 import './Navigation.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navigation() {
+
+  const location = useLocation();
+
   return (
     <nav className="navigation">
-      <ul className="navigation__list">
-        <li><Link to="/signup" className="navigation__link">Регистрация</Link></li>
-        <li><Link to="/signin" className="navigation__link navigation__link_signin">Войти</Link></li>
-      </ul>
+      {location.pathname === "/" ? (
+        <ul className="navigation__list_main">
+          <li class="navigation__item">
+            <Link to="/signup" className="navigation__link navigation__link_place_main">Регистрация</Link>
+          </li>
+          <li class="navigation__item">
+            <Link to="/signin" className="navigation__link navigation__link_place_main navigation__link_target_signin">Войти</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="navigation__list_other">
+          <li class="navigation__item">
+            <Link to="/movies" className="navigation__link navigation__link_place_other navigation__link_target_movies">Фильмы</Link>
+          </li>
+
+          <li class="navigation__item">
+            <Link to="/saved-movies" className="navigation__link navigation__link_place_other navigation__link_target_saved-movies">Сохранённые фильмы</Link>
+          </li>
+
+          <li class="navigation__item navigation__item_target_profile">
+            <Link to="/profile" className="navigation__link navigation__link_place_other navigation__link_target_profile">
+              Аккаунт <div className="navigation__profile-icon" > </div>
+            </Link></li>
+
+        </ul>
+      )
+      }
     </nav >
+
   );
 }
+
 
 export default Navigation;
