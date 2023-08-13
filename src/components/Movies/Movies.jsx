aca3d23a52fbcbd275606767f68be784d4d5b263
Artem Sibisov <artsib7777@yandex.ru>
2023-08-14 02:52:05 +0800

    feat: продолжить выполнение верстки компонента Movies: создать и подключить к нему компонент SearchForm, выполнить компонент FilterCheckbox и подключить его к SearchForm

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,13 @@
 import './Movies.css'
 
+import SearchForm from '../SearchForm/SearchForm';
+
 function Movies() {
   return (
-    <>
+    <main className="movies">
+      <SearchForm />
 
-    </>
+    </main>
   );
 }
 
---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,29 @@
+import './SearchForm.css'
+
+import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+
+function SearchForm() {
+
+  return (
+
+    <div className="search-form section">
+
+      <form className="search-form__form">
+
+        <fieldset className="search-form__input-container">
+          <input className="search-form__input" type="text" name="movie" placeholder="Фильм" required />
+          <button className="search-form__submit-btn" type="submit"></button>
+        </fieldset>
+
+      </form>
+
+      <div className="search-form__filter">
+        <FilterCheckbox />
+        <p className="search-form__filter-caption">Короткометражки</p>
+      </div>
+
+    </div >
+  );
+}
+
+export default SearchForm;
