5a0702ba692cf7233419a2dd9699ff50f6d90b0a
Artem Sibisov <artsib7777@yandex.ru>
2023-08-15 18:23:48 +0800

    feat: выполнить верстку компонента MoviesMore для компонента Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,13 +2,14 @@ import './Movies.css';
 
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import MoviesMore from '../MoviesMore/MoviesMore';
 
 function Movies() {
   return (
     <main className="movies">
       <SearchForm />
       <MoviesCardList />
-
+      <MoviesMore />
     </main>
   );
 }
---
+++ src/components/MoviesMore/MoviesMore.jsx
@@ -0,0 +1,12 @@
+import './MoviesMore.css'
+
+function MoviesMore() {
+  return (
+    <div className="movies-more section">
+      <button className="movies-more__btn">Ещё</button>
+    </div>
+
+  );
+}
+
+export default MoviesMore;
