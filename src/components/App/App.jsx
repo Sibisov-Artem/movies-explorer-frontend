596d5a17c749b0c52d101a337214ed5eef86f65b
Artem Sibisov <artsib7777@yandex.ru>
2023-08-16 03:23:49 +0800

    feat: выполнить верстку компонента Profile и подключить его к роуту /profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,7 @@ import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/' element={<Main />} />
           <Route path='/movies' element={<Movies />} />
           <Route path='/saved-movies' element={<SavedMovies />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
 
         <Footer />
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,65 @@
+import './Profile.css';
+
+import { useState } from 'react';
+
+function Profile() {
+
+  const [name, setName] = useState('Виталий');
+  const [email, setEmail] = useState('pochta@yandex.ru');
+
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <section className="profile section">
+
+      <div className="profile__container">
+        <h2 className="profile__title">{`Привет, ${name}!`}</h2>
+        <form className="profile__form" name="profile" onSubmit={onSubmit}>
+
+          <fieldset className="profile__input-container">
+            <label className="profile__label">Имя
+              <input className="profile__input"
+                type="text"
+                name="name"
+                placeholder="Имя"
+                required
+                minLength="2" maxLength="50"
+                id="profileName"
+                onChange={handleChangeName}
+                value={name}
+              />
+            </label>
+
+            <label className="profile__label">E-mail
+              <input className="profile__input profile__input_el_description"
+                type="text"
+                name="info"
+                placeholder="E-mail"
+                required
+                minLength="2" maxLength="50"
+                id="profileDescription"
+                onChange={handleChangeEmail}
+                value={email}
+              />
+            </label>
+          </fieldset>
+
+          <button className="profile__submit-btn" type="submit">Редактировать</button>  {/*submitText*/}
+        </form>
+        <button className="profile__close-btn" type="button">Выйти из аккаунта</button>
+      </div>
+
+    </section>
+  );
+}
+
+export default Profile;
