5945ccc3f3e5dcfa3c70fa7a8c797443f741bf7d
Artem Sibisov <artsib7777@yandex.ru>
2023-08-22 01:27:14 +0800

    feat: проследить чтобы все ссылки куда-то вели

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -14,7 +14,7 @@ function PageNotFound() {
     <div className="page-not-found">
       <h2 className="page-not-found__title">404</h2>
       <p className="page-not-found__subtitle">Страница не найдена</p>
-      <Link onClick={goBack} className="page-not-found__link">Назад</Link>
+      <Link onClick={goBack} className="page-not-found__link hover">Назад</Link>
     </div>
   );
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,19 +6,19 @@ function Portfolio() {
       <h4 className="portfolio__header">Портфолио</h4>
       <ul className="portfolio__list">
         <li className="portofilo__item">
-          <a className="portfolio__link hover" href="#">
+          <a className="portfolio__link hover" href="https://github.com/Sibisov-Artem/how-to-learn" target="_blank">
             Статичный сайт
             <div className="portfolio__link-arrow"></div>
           </a>
         </li>
         <li className="portofilo__item">
-          <a className="portfolio__link hover" href="#">
+          <a className="portfolio__link hover" href="https://github.com/Sibisov-Artem/russian-travel" target="_blank">
             Адаптивный сайт
             <div className="portfolio__link-arrow"></div>
           </a>
         </li>
         <li className="portofilo__item">
-          <a className="portfolio__link hover" href="#">
+          <a className="portfolio__link hover" href="https://github.com/Sibisov-Artem/react-mesto-auth" target="_blank">
             Одностраничное приложение
             <div className="portfolio__link-arrow"></div>
           </a>
