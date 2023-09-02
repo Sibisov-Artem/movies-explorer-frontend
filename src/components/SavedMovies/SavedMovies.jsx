a4fc6d9999a710c192a59069f94987ade5c56088
Artem Sibisov <artsib7777@yandex.ru>
2023-09-03 02:28:46 +0800

    fix: after review - убрать кнопку "Ещё" для сохраненных фильмов

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,10 +1,7 @@
 import './SavedMovies.css';
 
-import { useState, useEffect } from 'react';
-
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import MoviesMore from '../MoviesMore/MoviesMore';
 import Preloader from '../Preloader/Preloader'
 
 
@@ -18,45 +15,6 @@ function SavedMovies({
   isLoading,
   preloaderMessage,
 }) {
-
-  const [cardForDisplay, setCardForDisplay] = useState([])
-
-  // // при разрешении экрана >= 1280 показывать первоначально 12шт, потом добавлять по 3шт
-  // // при разрешении экрана < 1280 показывать первоначально 8шт, потом добавлять по 2шт
-  // // при разрешении экрана < 600 показывать первоначально 5шт, потом добавлять по 2шт
-  function showQuantityOnSize() {
-    if (window.innerWidth >= 1280) {
-      setCardForDisplay(12)
-    } else if (window.innerWidth < 600) {
-      setCardForDisplay(5)
-    } else {
-      setCardForDisplay(8)
-    }
-  }
-
-  useEffect(() => {
-    showQuantityOnSize();
-  }, []
-  )
-
-  //слушатель resize для синхронного изменения кол-ва к показу
-  //в зависимости от изменнеия разрешения с ограничением по времени setTimeout
-  useEffect(() => {
-    setTimeout(() => {
-      window.addEventListener('resize', showQuantityOnSize);
-    }, 1000);
-  }, []);
-
-  function loadMore() {
-    console.log(123)
-    if (window.innerWidth >= 1280) {
-      setCardForDisplay(cardForDisplay + 3)
-    } else if (window.innerWidth < 600) {
-      setCardForDisplay(cardForDisplay + 2)
-    } else {
-      setCardForDisplay(cardForDisplay + 2)
-    }
-  }
 
   return (
     <main className="saved-movies">
@@ -81,11 +39,6 @@ function SavedMovies({
             <MoviesCardList
               movieCards={movieCards}
               onMovieCardLikeOff={onMovieCardLikeOff}
-              cardForDisplay={cardForDisplay}
-            />
-            <MoviesMore
-              handleLoadClick={loadMore}
-              isMoreActive={movieCards.length > cardForDisplay ? true : false}
             />
           </>)}
 
